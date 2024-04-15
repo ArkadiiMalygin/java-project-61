baae29f360e056aacd11b081cf5f5ce370c12d27
Arkadii <arkadii.malygin@gmail.com>
2024-04-15 22:08:34 +0300

    it gets name and greets u

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,13 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void userData () {
+        Scanner info = new Scanner(System.in);
+        System.out.println("May I have your name?");
+
+        String userName = info.nextLine();
+        System.out.println("Hello, " + userName);
+    }
+}
